cd0b845e47cddf88313ec1f60f2c845cca0f9243
Stas <sieliseev@gmail.com>
2017-05-24 13:17:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stas/criminalintent/CrimeFragment.java
@@ -0,0 +1,11 @@
+package com.example.stas.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Stas on 24.05.2017.
+ */
+
+public class CrimeFragment extends Fragment {
+    
+}
