d948962fb741a0bfa4477fe80fa694af7c7c1442
Stas <sieliseev@gmail.com>
2017-06-08 12:48:59 +0300

    10 chapter

--- app/src/main/java/com/example/stas/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/stas/criminalintent/CrimeActivity.java
@@ -1,8 +1,20 @@
 package com.example.stas.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID = "com.example.stas.criminalintent.crime_id";
+
+    public static Intent newIntent (Context packageContext, UUID crimeID){
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/example/stas/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/stas/criminalintent/CrimeListFragment.java
@@ -1,14 +1,18 @@
 package com.example.stas.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,17 +48,40 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class SimpleCrimeHolder extends CrimeHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public SimpleCrimeHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        private Crime mCrime;
+
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private ImageView mSolvedImageView;
+
+        private CrimeHolder(LayoutInflater inflater, ViewGroup parent, int layout) {
+            super(inflater.inflate(layout, parent, false));
+
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
         }
-/*
+
+        protected void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(getFormat());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
+        }
+
+        private CharSequence getFormat() {
+            return DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate());
+        }
+
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
-        }*/
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getID());
+            startActivity(intent);
+        }
     }
 
     // add the second viewHolder with the button police
@@ -62,8 +89,8 @@ public class CrimeListFragment extends Fragment {
 
         private Button mPoliceButton;
 
-        public SeriousCrimeHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.list_item_crime_police, parent, false));
+        private SeriousCrimeHolder(LayoutInflater inflater, ViewGroup parent, int layout) {
+            super(inflater, parent, layout);
 
             mPoliceButton = (Button) itemView.findViewById(R.id.button_police);
         }
@@ -90,8 +117,8 @@ public class CrimeListFragment extends Fragment {
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
 
-        private final int VIEW_TYPE_SIMPLE = 1;
-        private final int VIEW_TYPE_SERIOUS = 2;
+        private final int VIEW_TYPE_SIMPLE = R.layout.list_item_crime;
+        private final int VIEW_TYPE_SERIOUS = R.layout.list_item_crime_police;
 
         private List<Crime> mCrimes;
 
@@ -113,11 +140,11 @@ public class CrimeListFragment extends Fragment {
 
             switch (viewType) {
                 case VIEW_TYPE_SIMPLE:
-                    return new SimpleCrimeHolder(layoutInflater, parent);
+                    return new CrimeHolder(layoutInflater, parent, viewType);
                 case VIEW_TYPE_SERIOUS:
-                    return new SeriousCrimeHolder(layoutInflater, parent);
+                    return new SeriousCrimeHolder(layoutInflater, parent, viewType);
                 default:
-                    return new SimpleCrimeHolder(layoutInflater, parent);
+                    return new CrimeHolder(layoutInflater, parent, viewType);
             }
 
         }
